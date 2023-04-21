ad7e030be2f86d249372bd8a81f03b628e0c793f
Parminder Singh <ps829220@gmail.com>
2023-04-21 14:29:14 +0530

    Add skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import Receipes from './Components/Receipes/Receipes';
-// import SearchReceipe from './Components/SearchReceipe/SearchReceipe';
 
 function App() {
   return (
       <>
-        {/* <SearchReceipe /> */}
         <Receipes />
       </>
   );
